9ad204fffcac572f43ce7345bdfae83364bd276f
wiserdor <vgibsonsg@gmail.com>
2020-04-05 02:58:33 +0300

    “init”

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,24 +7,8 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import { create } from "jss";
-import rtl from "jss-rtl";
-import JssProvider from "react-jss/lib/JssProvider";
-import { createGenerateClassName, jssPreset } from "@material-ui/core/styles";
 
-// Configure JSS
-const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 
-// Custom Material-UI class name generator.
-const generateClassName = createGenerateClassName();
-
-function RTL(props) {
-  return (
-    <JssProvider jss={jss} generateClassName={generateClassName}>
-      {props.children}
-    </JssProvider>
-  );
-}
 export default function Details({ cart }) {
   const [open, setOpen] = React.useState(false);
 
