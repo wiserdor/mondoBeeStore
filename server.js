5f3aa4909a0a4bb6e2489bb55911458ed1376162
dor-dcoya <dor@dcoya.com>
2020-04-05 20:57:35 +0300

    avocado

--- server.js
+++ server.js
@@ -9,13 +9,19 @@ const cors = require('cors')
 // Import routes
 const homeRouter = require('./routes/home')
 // Setup default port
-const PORT =  8080
+const PORT = process.env.PORT || 8080
 // Create express app
 const app = express()
 // Implement middleware
 app.use(cors())
+app.use(helmet())
+app.use(compression())
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
+
 if (process.env.NODE_ENV && process.env.NODE_ENV !== 'development') {
     app.get('*', (req, res) => {
       res.sendFile('build/index.html', { root: __dirname })
