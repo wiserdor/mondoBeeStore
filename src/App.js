bd389dd012dbed9335299631b7d7f0dcede2bd16
wiserdor <vgibsonsg@gmail.com>
2020-04-05 03:41:35 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import Container from '@material-ui/core/Container';
+import Grid from "@material-ui/core/Grid";
 import Catalog from "./components/Catalog";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Grid container style={{padding:10}}>
-        <Typography variant="h3" component="h2">
+      <Grid container align="center" style={{padding:10,textAlign:"center"}}>
+      <Grid item xs={12}>
+        <Typography variant="h3" style={{textAlign:"center"}} component="h2">
           החנות של מונדו
         </Typography>
+        </Grid>
+        <Grid item xs={12}>
         <Typography variant="h6" component="h4" style={{ marginBottom: 40 }}>
          מבחר רב של פירות העונה המשתנים בהתאם לסחורה בשוק.
          לפרטים נוספים התקשרו: מונדו 054-3300800
         </Typography>
+        </Grid>
         <Catalog />
       </Grid>
     </div>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -14,6 +14,7 @@ import Details from "./Details"
 
 const Cart = ({ cart, deleteFromCart }) => {
   return (
+    <Grid align="center" xs={12}>
     <div style={{width: '100%',
     maxWidth: 360}}>
       <Typography variant="h6">סיכום הזמנה</Typography>
@@ -39,6 +40,7 @@ const Cart = ({ cart, deleteFromCart }) => {
         {cart.length ? <Details />:null}
       </div>
     </div>
+    </Grid>
   );
 };
 
