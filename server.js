9962a03130cc5e9943f677d39e48de5efb846363
dor-dcoya <dor@dcoya.com>
2020-04-05 21:02:02 +0300

    avocado

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/build'))
 
 if (process.env.NODE_ENV && process.env.NODE_ENV !== 'development') {
     app.get('*', (req, res) => {
