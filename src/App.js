a922c869e1a065c414923d411369e20960f3c176
dor-dcoya <dor@dcoya.com>
2020-04-06 14:54:06 +0300

    avocado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
+import Divider from '@material-ui/core/Divider';
 
 import Catalog from "./components/Catalog";
 import "./App.css";
@@ -21,13 +22,14 @@ function App() {
       >
       <img src={logo} className="logo-background"/>
         <Grid item xs={12}>
-          <Typography variant="h4" component="h4" >
+          <Typography variant="h4" component="h4" style={{ marginTop: 20 }} >
             מבחר רב של פירות העונה המשתנים בהתאם לסחורה בשוק 
           </Typography>
-          <Typography variant="h6" component="h4" style={{ marginBottom: 40 }}>
+          <Typography variant="h6" component="h4" style={{ marginBottom: 20 }}>
             משלוחים לישובי לב השרון, תל מונד,קדימה-צורן ואבן יהודה
           </Typography>
         </Grid>
+        <Divider style={{marginBottom:40}} />
         <Catalog />
         <ToastContainer
         position="bottom-left"
