60b9ffc05498d2d21eb266f2c4bca25026a32c51
dor-dcoya <dor@dcoya.com>
2020-04-11 16:37:16 +0300

    added db

--- api/db/index.js
+++ api/db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 const pool = new Pool({
-  connectionString:process.env.DATABASE_URL+"?sslmode=require",
-  ssl: { rejectUnauthorized: false },
+  connectionString:process.env.DATABASE_URL,
+  ssl: true,
 });
 
 exports.getCatalog = async () => {
