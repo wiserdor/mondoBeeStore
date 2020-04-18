9584915d7ca3ec1594c167d3ce492113c8b68ded
dor-dcoya <dor@dcoya.com>
2020-04-18 21:49:45 +0300

    added .env file

--- api/controllers/home-controller.js
+++ api/controllers/home-controller.js
@@ -6,14 +6,19 @@ const mailjet = require("node-mailjet").connect(
 );
 
 exports.send = async (req, res) => {
-    /**
-     * Create an order and send mail to the owners
-     */
+  /**
+   * Create an order and send mail to the owners
+   */
   const j = req.body;
   // Save to db
   addOrder(j);
 
   // Send as mail
+  let to_mails;
+  const mail_arr=[];
+  if (process.env.TO_EMAILS) to_mails = process.env.TO_EMAILS.split(" ");
+  to_mails.forEach(m=>mail_arr.push({Email:m}))
+  console.log(mail_arr)
   const request = mailjet.post("send", { version: "v3.1" }).request({
     Messages: [
       {
@@ -21,20 +26,7 @@ exports.send = async (req, res) => {
           Email: "vgibsonsg@gmail.com",
           Name: "Mondo",
         },
-        To: [
-          {
-            Email: "Adigolan0910@gmail.com",
-            Name: "Adi",
-          },
-          {
-            Email: "ronabasmat@gmail.com",
-            Name: "Rona",
-          },
-            // {
-            //   Email: "vgibsonsg@gmail.com",
-            //   Name: "Mondo",
-            // },
-        ],
+        To:mail_arr,
         Subject: "הזמנה חדשה בחנות של מונדו",
         HTMLPart:
           `<body dir="rtl"><h1>הזמנה מ${j.details.name}</h1>` +
@@ -67,25 +59,26 @@ exports.send = async (req, res) => {
             j.details.name
           }</div><div>טלפון: ${j.details.phone} </div><div> עיר: ${
             j.details.city
-          }</div><div> כתובת: ${j.details.address}</div><div> הערות: ${j.details.notes || ''}</div></body>`,
+          }</div><div> כתובת: ${j.details.address}</div><div> הערות: ${
+            j.details.notes || ""
+          }</div></body>`,
       },
     ],
   });
   request
     .then((r) => {
-        res.status(200).send("ok")
+      res.status(200).send("ok");
     })
-    .error((e) => res.status(500).send("an error occur"))
+    .error((e) => res.status(500).send("an error occur"));
 };
 
 exports.catalog = async (req, res) => {
-  try{
+  try {
     const cat = await getCatalog();
     res.status(200).send(cat.rows);
-  } catch (err){
-    res.status(500).send("error")
+  } catch (err) {
+    res.status(500).send("error");
   }
-  
 };
 
 // exports.addToCatalog = async (req, res) => {
@@ -96,4 +89,3 @@ exports.catalog = async (req, res) => {
 //       res.status(500).send(req.body)
 //     }
 //   };
-  
