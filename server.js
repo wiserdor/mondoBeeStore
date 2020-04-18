3ee8906b80639e260ef025c0e87592076aa4979d
dor-dcoya <dor@dcoya.com>
2020-04-18 21:13:50 +0300

    added .env file

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 // Import express framework
 const express = require('express')
 // Import middleware
@@ -24,7 +26,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(bodyParser.json())
-console.log(process.env.DATABASE_URL)
+console.log(process.env.DB_URL)
 if (process.env.NODE_ENV && process.env.NODE_ENV !== 'development') {
     app.use(express.static(__dirname + '/build'))
 
